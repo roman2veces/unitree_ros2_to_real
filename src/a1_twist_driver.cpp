23975322dc72aff03a0988400e77bd67bdbba4ae
roman2veces <romanroman2705@gmail.com>
2023-07-11 17:28:53 +0000

    start twist driver

--- src/a1_twist_driver.cpp
+++ src/a1_twist_driver.cpp
@@ -6,8 +6,46 @@
 #include <boost/thread/mutex.hpp>
 #include "ros2_unitree_legged_msgs/msg/high_cmd.h"
 #include "ros2_unitree_legged_msgs/msg/high_state.h"
-
 #include "convert.h"
+
+#include <geometry_msgs/msg/twist.hpp>
+
+class A1TwistDriver : public rclcpp::Node
+{
+  public:
+    A1TwistDriver() : Node("a1_twist_driver") {
+      twist_subscriber_ = this->create_subscription<geometry_msgs::msg::Twist>(
+      "cmd_vel", 10, std::bind(&A1TwistDriver::topic_callback, this, std::placeholders::_1));
+    }
+    
+    // template<typename TLCM>
+    // void* update_loop(void* param)
+    // {
+    //     TLCM *data = (TLCM *)param;
+    //     while(rclcpp::ok){
+    //         data->Recv();
+    //         usleep(2000);
+    //     }
+    // }
+    
+    UNITREE_LEGGED_SDK::LCM roslcm = UNITREE_LEGGED_SDK::LCM(UNITREE_LEGGED_SDK::HIGHLEVEL);
+    UNITREE_LEGGED_SDK::HighCmd unitree_high_cmd_ = {0};
+
+
+  private:
+    ros2_unitree_legged_msgs::msg::HighState ros_high_state_;
+
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr twist_subscriber_;
+    
+    void topic_callback(const geometry_msgs::msg::Twist::SharedPtr msg)
+    {
+        ros2_unitree_legged_msgs::msg::HighCmd ros_high_cmd_;
+        ros_high_cmd_.forward_speed = msg->linear.x;
+
+        this->unitree_high_cmd_ = ToLcm(ros_high_cmd_, this->unitree_high_cmd_);
+        // roslcm.Send(unitree_high_cmd_);
+    }
+};
 
 template<typename TLCM>
 void* update_loop(void* param)
@@ -19,8 +57,45 @@ void* update_loop(void* param)
     }
 }
 
+int main(int argc, char * argv[])
+{
+    // UNITREE_LEGGED_SDK::LeggedType rname = UNITREE_LEGGED_SDK::LeggedType::A1;
+    // UNITREE_LEGGED_SDK::LCM roslcm(UNITREE_LEGGED_SDK::HIGHLEVEL);
+    
+    std::cout << "WARNING: Control level is set to HIGH-level." << std::endl
+              << "Make sure the robot is standing on the ground." << std::endl
+              << "Press Enter to continue..." << std::endl;
+    std::cin.ignore();
+    // RCLCPP_INFO(a1_twist_driver->get_logger(), "before everything");
+    // std::cout << "WHAT UP" << std::endl;
+    rclcpp::init(argc, argv);
+    auto a1_twist_driver = std::make_shared<A1TwistDriver>();
+    // RCLCPP_INFO(a1_twist_driver->get_logger(), "before subscribe");
+    // std::cout << "WHAT UP 2" << std::endl;
+    a1_twist_driver->roslcm.SubscribeState();
+
+    // RCLCPP_INFO(a1_twist_driver->get_logger(), "node");
+    pthread_t tid;
+    pthread_create(&tid, NULL, update_loop<UNITREE_LEGGED_SDK::LCM>, &a1_twist_driver->roslcm);
+    
+    // RCLCPP_INFO(a1_twist_driver->get_logger(), "thread");
+
+    // auto node = rclcpp::Node::make_shared("node_ros2_walk_example");
+    rclcpp::spin(a1_twist_driver);
+    // rclcpp::WallRate loop_rate(500);
+    // rclcpp::executors::SingleThreadedExecutor executor;
+    // executor.add_node(node);
+
+    RCLCPP_INFO(a1_twist_driver->get_logger(), "final");
+
+    // mainHelper<UNITREE_LEGGED_SDK::HighCmd, UNITREE_LEGGED_SDK::HighState, UNITREE_LEGGED_SDK::LCM>(argc, argv, roslcm);
+    return 0;
+}
+
+
 // TCmd = Type command
 // TState = Type state
+// TODO: change function name and add it to this node class
 template<typename TCmd, typename TState, typename TLCM>
 int mainHelper(int argc, char *argv[], TLCM &roslcm)
 {
@@ -48,7 +123,7 @@ int mainHelper(int argc, char *argv[], TLCM &roslcm)
     pthread_t tid;
     pthread_create(&tid, NULL, update_loop<TLCM>, &roslcm);
 
-    while (rclcpp::ok()){
+    while (rclcpp::ok()) {
         motiontime = motiontime + 2;
         roslcm.Get(RecvHighLCM);
         RecvHighROS = ToRos(RecvHighLCM);
@@ -132,10 +207,10 @@ int mainHelper(int argc, char *argv[], TLCM &roslcm)
     return 0;
 }
 
-int main(int argc, char *argv[]) {
-    std::string robot_name;
-    UNITREE_LEGGED_SDK::LeggedType rname = UNITREE_LEGGED_SDK::LeggedType::A1;
+// int main(int argc, char *argv[]) {
+//     std::string robot_name;
+//     UNITREE_LEGGED_SDK::LeggedType rname = UNITREE_LEGGED_SDK::LeggedType::A1;
 
-    UNITREE_LEGGED_SDK::LCM roslcm(UNITREE_LEGGED_SDK::HIGHLEVEL);
-    mainHelper<UNITREE_LEGGED_SDK::HighCmd, UNITREE_LEGGED_SDK::HighState, UNITREE_LEGGED_SDK::LCM>(argc, argv, roslcm);
-}
+//     UNITREE_LEGGED_SDK::LCM roslcm(UNITREE_LEGGED_SDK::HIGHLEVEL);
+//     mainHelper<UNITREE_LEGGED_SDK::HighCmd, UNITREE_LEGGED_SDK::HighState, UNITREE_LEGGED_SDK::LCM>(argc, argv, roslcm);
+// }
